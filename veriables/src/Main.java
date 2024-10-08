59a168af15ec476236ef01116ec1ddb6662c9df7
AkSysevo <161967650+AkSysevo@users.noreply.github.com>
2024-10-08 16:50:30 +0300

    Add files via upload

---
+++ veriables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // java case sensitive (büyük-küçük harf duyarlı)dildir.
+        //camelCasing
+        //reusebility=tekrar tekrar kullanılabilirlik
+        System.out.println("Öğrenci sayım :10");
+
+        int ogrenciSayisi = 11;
+        String metin = "Öğrenci sayım :";
+        System.out.println(metin + ogrenciSayisi);
+
+    }
+}
